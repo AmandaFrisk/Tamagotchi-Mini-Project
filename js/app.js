8a42cc56aa2c14b93bd23f250d5e2f6b9d56d674
AmandaFrisk <amanda.frisk33@gmail.com>
2022-07-05 18:50:40 -0600

    Intro alerts and name prompt complete

---
+++ js/app.js
@@ -0,0 +1,5 @@
+alert("Welcome to your first day of Pokemon pre-trainer. Click OK to begin your training. ")
+const name = prompt("What's your name?"); 
+const Professor = "Professor Amanda"
+console.log(name);
+alert(`It is great to meet you ${name}! I am ${Professor}. In this pre-training course you will learn to take care of your Pokemon.`)
