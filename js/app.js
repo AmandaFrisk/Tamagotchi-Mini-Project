e0325a3b1335d6d35192327d180ec2070b42ff21
AmandaFrisk <amanda.frisk33@gmail.com>
2022-07-08 17:06:37 -0600

    css added

--- js/app.js
+++ js/app.js
@@ -22,7 +22,9 @@ let hungerScore= document.querySelector('.hungerScore')
 let sleepinessScore = document.querySelector('.sleepinessScore')
 let boredomScore = document.querySelector('.boredomScore')
 let ageTracking = document.querySelector('#Age')
-
+let grabButtons = document.querySelector('#buttons')
+let grabFoodImage = document.querySelector('#Food-Image')
+let grabBackgroundImage = document.body.style.backgroundImage
 class Pokemon {
     constructor( name, hunger, sleepiness, boredom, age){ 
         this.name = name                
@@ -36,22 +38,23 @@ class Pokemon {
      let startVar = setInterval(() => {
             
            if(this.hunger >= 1 && this.sleepiness >= 1 && this.boredom >= 1 ){
-            this.hunger--
+            this.hunger --
             hungerScore.innerHTML = this.hunger
             console.log(hungerScore)
-            this.sleepiness--
+            this.sleepiness --
             sleepinessScore.innerText = this.sleepiness
             console.log(sleepinessScore)
-            this.boredom--
+            this.boredom --
             boredomScore.innerText = this.sleepiness
             console.log(boredomScore)
             }
             else if(this.hunger <= 0 || this.sleepiness <= 0 || this.boredom <= 0 ){
                 alert("Oh dear, the Pokemon has fainted!")
                 clearInterval(startVar)
-                document.body.style.backgroundImage="url('https://images.unsplash.com/photo-1588421357574-87938a86fa28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')"
-                document.querySelector('#picSwap').src='../images/Game-Over.gif'
-                document.querySelector('#buttons').remove()//????? NEEDS  TO STOP setTimeOut? // Visual for fainting?
+               document.body.style.backgroundImage="url('https://images.unsplash.com/photo-1588421357574-87938a86fa28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')"
+               document.querySelector('picSwap').src='../images/Game-Over.gif'
+               
+               grabButtons.remove()//????? NEEDS  TO STOP setTimeOut? // Visual for fainting?
             } else {
                 console.log("conditionals not working")
             }
@@ -64,11 +67,21 @@ class Pokemon {
          feedPokemon(){
             console.log("fed pokemon")
              this.hunger++ 
-            hungerScore.innerHTML = this.hunger
-            console.log(hungerScore)
-
-            //some kind of visual or sound needs to happen when fed
-
+            hungerScore.innerHTML = this.hunger 
+         //console.log(hungerScore)
+           function addFoodPic(){
+            console.log("inside addFoodPic")
+            let berryDiv = document.createElement('div')
+            berryDiv.setAttribute('id', 'berryDiv')
+             grabFoodImage.appendChild(berryDiv)
+            let berryImageTag =document.createElement('img')
+            berryImageTag.setAttribute('id',"berryImage") 
+            berryDiv.appendChild(berryImageTag)
+            berryImageTag.src= '../images/Purple-Berry.png'
+            //some kind of visual or sound needs to happen when fed 
+            
+            }
+            setTimeout(addFoodPic, 1000) //????Isn't working
           }
                                                                                
         
@@ -91,10 +104,11 @@ class Pokemon {
 
         lightsOff(){
             console.log("lightsOff")
+         
         document.body.style.backgroundImage="url('../images/DarkForest.jpeg')"
         // ZZZ image change too?
         function lightsOn(){
-            document.body.style.backgroundImage="url('../images/LightForest.jpeg')" 
+         document.body.style.backgroundImage="url('../images/LightForest.jpeg')" 
       }
       setTimeout(lightsOn, 1000)
         
@@ -129,18 +143,16 @@ class Pokemon {
 }
 
 //new instance
-let selectedPokemon = new Pokemon(`${pokemonName}`, 5, 12, 8, 0)
+let selectedPokemon = new Pokemon(`${pokemonName}`, 10, 12, 13, 0)
 console.log(selectedPokemon);
 
 
 
 //EVENT LISTENERS
 document.getElementById('start').addEventListener('click',(e)=>{ selectedPokemon.ageUp(),selectedPokemon.Start()}) 
-document.getElementById('start').addEventListener('click',(e)=>{ selectedPokemon.Start()}) 
 document.getElementById('feed').addEventListener('click', (e)=>{selectedPokemon.feedPokemon()}) // when #feed btn clicks feedPokemon () - hunger goes down
 document.getElementById('lights').addEventListener('click',(e)=>{ selectedPokemon.sleepyPokemon()}) // when #lights btn clicks sleepyPokemon()-sleepiness goes down
 document.getElementById('play').addEventListener('click', (e)=>{selectedPokemon.Played()}) //when #play btn clicks bordom goes down
-
 document.getElementById('lights').addEventListener('click',(e)=>{ selectedPokemon.lightsOff()}) //when #lights btn clicked lightsOff()- darkened image change
 
 //Brain Dump
